aec7de58b0d6eb0dead48d4ae258b15a39076f2b
Philippe <philippebs92@hotmail.com>
2019-05-01 15:03:02 -0300

    Imprimindo porcentagem de saida dos numeros

--- downloadLotoCaixa.py
+++ downloadLotoCaixa.py
@@ -8,12 +8,14 @@ def download_descompactar():
 	request = requests.get('http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip')
 	with open('D_lotfac.zip', 'wb') as code:
 		code.write(request.content)
-
-	zip = zipfile.ZipFile(BASE_PATH + '/D_lotfac.zip')
-	#zip.write(BASE_PATH, compress_type=zipfile.ZIP_DEFLATED)
 	
 	zip.extractall(BASE_PATH)
 	zip.close()
+	
+def write_zip():
+	BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+	zip = zipfile.ZipFile(BASE_PATH + '/D_lotfac.zip')
+	zip.write(BASE_PATH, compress_type=zipfile.ZIP_DEFLATED)
 	
 
 if __name__ == '__main__':
--- numeros.py
+++ numeros.py
@@ -17,12 +17,37 @@ def rastreio_numeros(lista_numeros_escolhidos):
 		contador = 0
 		for numero_escolhido in lista_numeros_escolhidos:
 			if numero_escolhido in numeros_sorteados:
-				print(numeros_sorteados)
+				#print(numeros_sorteados)
 				contador+= 1
-		print(contador)
+		#print(contador)
+
+def contar_vezes_que_aparece(numero):
+	contador = 0
+	for numero_sorteado in todos_numeros:
+		if numero in numero_sorteado:
+			contador+=1
+	return contador
+
+def calcular_porcentagem():
+	numero = {valor : 0 for valor in range(1, 26)}
+	for chave in numero:
+		quantidade = contar_vezes_que_aparece(chave)
+		numero[chave] = quantidade
+		porcentagem = (quantidade * 100) / len(todos_numeros)
+		print(str(chave) + ' - ' + str(porcentagem) + '%')
+
+	[print(str(chave) + ' - ' + str(value)) for chave, value in numero.items()]
+
+	#key = max(numero, key=numero.get)
+	#quantidade = numero[key]
+
+	#porcentagem = (quantidade * 100) / len(todos_numeros)
+
+	#print('Maior quantidade de sorteios: ' + str(key) + ' - ' + str(quantidade) + ' : ' + str(porcentagem) + '%')
 
 if __name__ == '__main__':
 	popula_numeros()
+	calcular_porcentagem()
 	#print(todos_numeros)
 
 
